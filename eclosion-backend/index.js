04cdbf82562f2f77a83420e604a70e6db45824d2
Valentina Toledo <valentina.toledoporma@gmail.com>
2025-06-10 08:17:30 -0400

    Se integran las conexiones base de express para levantar servidor con peticion GET

--- eclosion-backend/index.js
+++ eclosion-backend/index.js
@@ -1,2 +1,19 @@
+const express = require('express');
+const PORT = 4001;
 
-console.log('holamundoxxx')
+//Creacion de server de express
+const app = express();
+
+//Ruta de prueba
+app.get('/', (req, res) => {
+    console.log('Se requiere /');
+    res.json({
+        ok: true
+    })
+})
+
+
+//Escucha de peticiones
+app.listen(PORT, () => {
+    console.log(`Servidor activo en puerto ${PORT}`);
+});
