32906f54b6517544f74374774cd0b7cad1aa982a
Trinidad Valenzuela <trinidad.valenzuela@genstudents.org>
2025-06-13 01:42:11 -0400

    aplicacion estilos en vista about us

--- eclosion-frontend/src/Components/About Us/About.jsx
+++ eclosion-frontend/src/Components/About Us/About.jsx
@@ -1,53 +1,35 @@
 import './about-style.css';
 import { Link } from 'react-router-dom';
+import reactLogo from '../../assets/Pictures/React-Logo-v2.png';
 
 
 function About() {
   return (
-    <section>
-      <div className="container">
-        <h1 className="about-title">¿Quiénes somos?</h1>
-        <p>
-          En Pulso Ecomarinos, creemos en la innovación y el compromiso. Nos
-          especializamos en el área de desarrollo web para ofrecer soluciones que
-          marcan la diferencia. Con un equipo apasionado y visión estratégica,
-          creamos experiencias que potencian a nuestros clientes y sus proyectos.
-        </p>
-        <p>
-          Transformamos ideas en experiencias digitales a través del desarrollo de
-          páginas web innovadoras y funcionales. Creemos en la creatividad, la
-          tecnología y la colaboración para ofrecer soluciones que destacan. Este
-          sitio existe porque queremos compartir nuestra pasión y mostrar al mundo
-          quiénes somos, qué hacemos y cómo podemos ayudarte a materializar tus
-          proyectos. Cada línea de código que escribimos refleja nuestra dedicación
-          y compromiso con la excelencia digital.
-        </p>
-        <div className="container-members">
-          <h2>Miembros del equipo</h2>
-          <div className="list-members">
-            <div className="members">
-              
-              <p>Esteban Madrid</p>
-            </div>
-            <div className="members">
-            
-              <p>Ruben Valdebenito</p>
-            </div>
-            <div className="members">
-              <p>Sebastián Guerra</p>
-            </div>
-            <div className="members">
-            
-              <p>Trinidad Valenzuela</p>
-            </div>
-            <div className="members">
-              
-              <p>Valentina Toledo</p>
-            </div>
-          </div>
+    <div>
+      <div className="about-intro-row row align-items-center">
+        <div className="col">
+          <h1 className="about-title">¿Quiénes somos?</h1>
+        </div>
+        <div className="col-auto">
+          <img className="about-logo" src={reactLogo} alt="React Logo" />
         </div>
       </div>
-    </section>
+      <p className="container-fluid parrafo-uno">
+        En Eclosión, creemos en la innovación y el compromiso. Nos
+        especializamos en el área de desarrollo web para ofrecer soluciones que
+        marquen la diferencia. Con un equipo apasionado por la tecnología y visión estratégica,
+        creamos experiencias que potencian a nuestros clientes y sus proyectos.
+      </p>
+      <p className="container-fluid parrafo-dos">
+        Transformamos ideas en experiencias digitales a través del desarrollo de
+        páginas web innovadoras y funcionales. Creemos en la creatividad, la
+        tecnología y la colaboración para ofrecer soluciones que destacan. Este
+        sitio existe porque queremos compartir nuestra pasión y mostrar al mundo
+        quiénes somos, qué hacemos y cómo podemos ayudarte a materializar tus
+        proyectos. Cada línea de código que escribimos refleja nuestra dedicación
+        y compromiso con la excelencia digital.
+      </p>
+    </div>
   );
 }
 
