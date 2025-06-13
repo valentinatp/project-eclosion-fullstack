d1e8799739c80de2d0f5bec2ff025fc555d63874
EstebanCML <esteban.madrid.83@gmail.com>
2025-06-13 16:28:59 -0400

    bugs de home y tamano navbar

--- eclosion-frontend/src/Components/Home/TextWelcome/TextWelcome.jsx
+++ eclosion-frontend/src/Components/Home/TextWelcome/TextWelcome.jsx
@@ -1,6 +1,7 @@
 const TextWelcome = () => {
     return (
         <>
+        <div className="padding-text-welcome">
             <section className="container-fluid">
                 <div className="row">
                     <div className="col">
@@ -8,7 +9,8 @@ const TextWelcome = () => {
                         <p className="text-justify">Descubre información clave sobre la contaminación en océanos, playas y ríos de Chile con nuestra plataforma interactiva. Explora datos actualizados, conoce el imapacto ambiental y encuentra formas de contribuir a un futuro más limpio. ¡Únete a nuestra comunidad y haz la diferencia!.</p>
                     </div>
                 </div>
-            </section>   
+            </section>\
+               </div>
         </>     
     )
 }
