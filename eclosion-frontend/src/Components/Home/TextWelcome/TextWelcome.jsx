43c603e7d6def436387794e8cc17143a9c936c1b
Trinidad Valenzuela <trinidad.valenzuela@genstudents.org>
2025-06-15 17:22:41 -0400

    migración about us a bootstrap y otros detalles

--- eclosion-frontend/src/Components/Home/TextWelcome/TextWelcome.jsx
+++ eclosion-frontend/src/Components/Home/TextWelcome/TextWelcome.jsx
@@ -5,12 +5,12 @@ const TextWelcome = () => {
             <section className="container-fluid">
                 <div className="row">
                     <div className="col">
-                        <h1 className="title-site mt-3">Bienvenidos a Eclosión</h1>
-                        <p className="text-justify">Descubre información clave sobre la contaminación en océanos, playas y ríos de Chile con nuestra plataforma interactiva. Explora datos actualizados, conoce el imapacto ambiental y encuentra formas de contribuir a un futuro más limpio. ¡Únete a nuestra comunidad y haz la diferencia!.</p>
+                        <h1 className="title-site mt-3 text-center">Bienvenidos a Eclosión</h1>
+                        <p className="text-center">Descubre información clave sobre la contaminación en océanos, playas y ríos de Chile con nuestra plataforma interactiva. Explora datos actualizados, conoce el imapacto ambiental y encuentra formas de contribuir a un futuro más limpio. ¡Únete a nuestra comunidad y haz la diferencia!.</p>
                     </div>
                 </div>
             </section>\
-               </div>
+        </div>
         </>     
     )
 }
