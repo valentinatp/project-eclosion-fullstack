b909faea5039450d2425159a7b794cd9ba898b0b
Trinidad Valenzuela <trinidad.valenzuela@genstudents.org>
2025-06-17 09:32:22 -0400

    cambios principales en about us, subida de imágenes al proyecto y otros cambios

--- eclosion-frontend/src/routes.jsx
+++ eclosion-frontend/src/routes.jsx
@@ -26,7 +26,7 @@ export const router = createBrowserRouter(
             <Route path="/about" element={<About />} />
             <Route path="/myprofile" element={<MyProfile />} />
             <Route path="/ejemplo" element={<h1>Hola ejemplo</h1>} />
-            <Route path="/createNewsPosts" element={<CreateNewsPosts  />} />
+            
         </Route>
     )
 )
