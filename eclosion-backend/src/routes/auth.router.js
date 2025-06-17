9aedfca27b82c2eb238d1c4a99991ed400f4a808
Valentina Toledo <valentina.toledoporma@gmail.com>
2025-06-17 10:56:38 -0400

    se modifica nombre de archivo controlador

--- eclosion-backend/src/routes/auth.router.js
+++ eclosion-backend/src/routes/auth.router.js
@@ -1,5 +1,5 @@
 const { registerUser, loginUser, userId } = require('../controllers/users.controller')
-const { createNews, refreshNews } = require('../controllers/feed.controller')
+const { createNews, refreshNews } = require('../controllers/newsPost.controller')
 const { Router } = require('express')
 const router = Router();
 
