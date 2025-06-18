a8e63276d2f58db77a7f7327073219163296224a
Trinidad Valenzuela <trinidad.valenzuela@genstudents.org>
2025-06-17 23:56:21 -0400

    fix estilos text welcome home

--- eclosion-frontend/src/Components/Home/TextWelcome/TextWelcome.jsx
+++ eclosion-frontend/src/Components/Home/TextWelcome/TextWelcome.jsx
@@ -4,9 +4,9 @@ const TextWelcome = () => {
         <div className="padding-text-welcome">
             <section className="container-fluid">
                 <div className="row">
-                    <div className="col">
-                        <h1 className="title-site mt-3 text-center">Bienvenidos a Eclosión</h1>
-                        <p className="text-center">Descubre información clave sobre la contaminación en océanos, playas y ríos de Chile con nuestra plataforma interactiva. Explora datos actualizados, conoce el imapacto ambiental y encuentra formas de contribuir a un futuro más limpio. ¡Únete a nuestra comunidad y haz la diferencia!.</p>
+                    <div className="col welcome-home">
+                        <h1 className="title-site mt-3 text-center">¡Bienvenidos a Eclosión!</h1>
+                        <p className="text-center home-intro-text">Descubre información clave sobre la contaminación en océanos, playas y ríos de Chile con nuestra plataforma interactiva. Explora datos actualizados, conoce el imapacto ambiental y encuentra formas de contribuir a un futuro más limpio. ¡Únete a nuestra comunidad y haz la diferencia!.</p>
                     </div>
                 </div>
             </section>\
