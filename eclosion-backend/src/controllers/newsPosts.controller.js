13abb31dee4225b105407025578db20e594a47dd
Valentina Toledo <valentina.toledoporma@gmail.com>
2025-06-17 22:30:35 -0400

    conexion y carga de noticias en feed

--- eclosion-backend/src/controllers/newsPosts.controller.js
+++ eclosion-backend/src/controllers/newsPosts.controller.js
@@ -6,7 +6,7 @@ const createNews = async (req, res) => {
     //Recibimos los campos del usuario
     const { author, category, title, bodyNews, dateNews, imageUser, imageNews, linkNews } = req.body
     //Validamos que los datos se inyecten correctamente
-    if ( !author || !category || !title || !bodyNews || !dateNews || !linkNews ) {
+    if ( !author || !category || !title || !bodyNews || !linkNews ) {
         //Si falta algun parametro requerido se indica el error al cliente
         res.status(400).json({
             message : "Faltan datos para la creacion del post",
@@ -31,14 +31,45 @@ const createNews = async (req, res) => {
     }
 }
 
+//Controlador consulta a la BD para actualizar noticias en feed
+const refreshNews = async (_, res) => {
+    try {
+        const newslist = await Newspost.find();
+        console.log(newslist);
 
+        res.status(200).json({
+            message: "Consulta realizada con exito",
+            status: 200,
+            data: {
+                newsPost : newslist,
+            }
+        })
+    } catch ( error ) {
+        console.log(error)
+        res.status(500).json({
+            message: "Error al crear un post",
+            error: error.message,
+        })
+    }
+}
 
 //Controlador consulta a la BD para actualizar noticias en feed
-const refreshNews = (req, res) => {
-    res.status(200).send("Esta es la ruta para recargar el Feed");
-}
+const getAllNews = async (_, res) => {
+  try {
+    const news = await Newspost.find({});
+    res.json(news);
+  } catch (error) {
+      res.status(500).json({
+          message: 'Error al obtener noticias',
+          error : error,
+      });
+  }
+};
+
+
 
 module.exports = {
     createNews,
     refreshNews,
+    getAllNews
 }
